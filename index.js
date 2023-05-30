f1d208b7b3f7d22b3cd590c015c071675bb5a917
Amantur Isaev <eunoianobu@protonmail.com>
2023-05-30 23:42:18 +0600

    saving progress
    progress being made with the blackjack game

---
+++ index.js
@@ -0,0 +1,29 @@
+let firstCard = 4;
+let secondCard = 7;
+let sum = firstCard + secondCard;
+
+if (sum <= 20) {
+    console.log("Do you want to draw another card? 😄")
+} else if(sum === 21) {
+    console.log("Woohoo! You've got a Blackjack! 😎")
+} else {
+    console.log("You're out! 😲")
+}
+
+//let age = 22;
+//
+//if(age < 21 ) {
+//    console.log ("You can not enter the club!")
+//} else {
+//    console.log("Welcome!")
+//}
+
+//let age = 24;
+//
+//if (age < 100) {
+//    console.log("Not eligible!");
+//} else if (age === 100) {
+//    console.log("Here is your birthday card from the King!");
+//} else {
+//    console.log("Not eligible, you've already gotten one!");
+//}
