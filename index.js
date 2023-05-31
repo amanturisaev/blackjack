5fabb807b2a10e93716ce2b273687910131334e6
Amantur Isaev <eunoianobu@protonmail.com>
2023-06-01 01:02:12 +0600

    saving progress with the blackjack game

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 let firstCard = 10;
-let secondCard = 11;
+let secondCard = 9;
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
 let message = "";
-
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+    sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
-        message = "Do you want to draw another card? 😄";
+        message = "Do you want to draw another card?";
     } else if(sum === 21) {
-        message = "Woohoo! You've got a Blackjack! 😎";
+        message = "You've got a Blackjack!";
         hasBlackjack = true;
         console.log(hasBlackjack)
     } else {
-        message = "You're out! 😲";
+        message = "You're out!";
         isAlive = false;
         console.log(isAlive)
     }
-    console.log(message);
+    messageEl.textContent = message;
 }
+
+function newCard() {
+    console.log("Drawing a new card from the deck!");
+    let anotherCard = 5;
+    sum += anotherCard;
+    startGame();
+}
+
 //let age = 22;
 //
 //if(age < 21 ) {
